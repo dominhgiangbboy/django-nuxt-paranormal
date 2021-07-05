e56ef261692c0b5ff3a7477edf0629c398e77a92
Đỗ Minh Giang <dominhgianga5nc@gmail.com>
2021-07-05 11:32:18 +0700

    Giang add

--- back_end_project/fukakachi/indexApp/urls.py
+++ back_end_project/fukakachi/indexApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CategoryView, FileDownloadDemo,FileUploadDemo
+from .views import CategoryView, FileDownloadDemo,FileUploadDemo,GetLink
 
 app_name = 'mainApp'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('category-list-get/', CategoryView.as_view(), name="category-list-get"),
     path('download-file/', FileDownloadDemo.as_view(), name="download-pdf"),
     path('upload-file/', FileUploadDemo.as_view(), name="upload-file"),
+    path('get-user-link/', GetLink.as_view(), name="upload-file"),
     
 ]
